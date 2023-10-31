afc396a757481e1fab28856ed8dec354680dff20
ShanuFarhan <shanumet@gmail.com>
2023-10-31 09:53:46 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiCallService } from './api-call.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Addtocart';
-}
+  Products:any=[]
+  constructor(private getApi:ApiCallService){}
+  ngOnInit(){
+
+    this.getApi.getPost().subscribe((res)=>{
+      console.log(res);
+      this.Products=res
+    })
+  }
+  }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {MatIconModule} from '@angular/material/icon';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {HttpClientModule } from '@angular/common/http'
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule,
+    FontAwesomeModule,
+    HttpClientModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
